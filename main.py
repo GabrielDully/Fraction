63f701632cb4cc56c8e833bbecc47e088b4a99a5
GabrielDully <gabrieldully@usp.br>
2024-02-13 13:58:19 -0300

    operation_component -> numerator
    Change of output variable names of the four basic operation (+ - * /) methods, e. g.,
    -FROM 'sum_numerator', we simplified TO 'numerator'.
    -FROM 'product_denominator', we simplified TO 'denominator'.

--- main.py
+++ main.py
@@ -15,35 +15,35 @@ class Fraction(object):
     def __add__(self, other_fraction):
 
         if(self.denominator == other_fraction.denominator):
-            sum_numerator = self.numerator + other_fraction.numerator
-            sum_denominator = self.denominator
+            numerator = self.numerator + other_fraction.numerator
+            denominator = self.denominator
         else:
-            sum_numerator = self.numerator*other_fraction.denominator + other_fraction.denominator*self.denominator
-            sum_denominator = self.denominator*other_fraction.denominator
+            numerator = self.numerator*other_fraction.denominator + other_fraction.denominator*self.denominator
+            denominator = self.denominator*other_fraction.denominator
         
-        return Fraction(sum_numerator, sum_denominator)
+        return Fraction(numerator, denominator)
 
     def __sub__(self, other_fraction):
 
         if(self.denominator == other_fraction.denominator):
-            difference_numerator = self.numerator - other_fraction.numerator
-            difference_denominator = self.denominator
+            numerator = self.numerator - other_fraction.numerator
+            denominator = self.denominator
         else:
-            difference_numerator = self.numerator*other_fraction.denominator - other_fraction.denominator*self.denominator
-            difference_denominator = self.denominator*other_fraction.denominator
+            numerator = self.numerator*other_fraction.denominator - other_fraction.denominator*self.denominator
+            denominator = self.denominator*other_fraction.denominator
         
-        return Fraction(difference_numerator, difference_denominator)
+        return Fraction(numerator, denominator)
     
     def __mul__(self, other_fraction):
         
-        product_numerator = self.numerator*other_fraction.numerator
-        product_denominator = self.denominator*other_fraction.denominator
+        numerator = self.numerator*other_fraction.numerator
+        denominator = self.denominator*other_fraction.denominator
 
-        return Fraction(product_numerator, product_denominator)
+        return Fraction(numerator, denominator)
     
     def __truediv__(self, other_fraction):
         
-        quotient_numerator = self.numerator*other_fraction.denominator
-        quotient_denominator = self.denominator*other_fraction.numerator
+        numerator = self.numerator*other_fraction.denominator
+        denominator = self.denominator*other_fraction.numerator
 
-        return Fraction(quotient_numerator, quotient_denominator) 
+        return Fraction(numerator, denominator) 
